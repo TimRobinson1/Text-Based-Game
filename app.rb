f721bec9422697f626e11f365f8baf9a62d4d79c
Timothy Robinson <timmyrobby@gmail.com>
2017-05-19 06:22:55 +0100

    Enables rack sessions

--- app.rb
+++ app.rb
@@ -1,12 +1,20 @@
 require 'sinatra'
 
 class Survival < Sinatra::Base
+  use Rack::MethodOverride
+  use Rack::Session::Pool
+
   get '/' do
     erb(:index)
   end
 
+  post '/game/new' do
+    session[:name] = params[:name]
+    redirect '/game'
+  end
+
   get '/game' do
-    @name = params[:name]
+    @name = session[:name]
     erb(:game)
   end
 end
