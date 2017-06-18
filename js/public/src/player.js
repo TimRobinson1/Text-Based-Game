61eb55582a01cccd02833c16ae9ebb24378d1558
Timothy Robinson <timmyrobby@gmail.com>
2017-06-18 12:17:16 +0100

    Player loses hunger points after one day

--- js/public/src/player.js
+++ js/public/src/player.js
@@ -1,5 +1,12 @@
 function Player() {
   this.health = 100;
+  this.hunger = 10;
+  this.thirst = 10;
+  this.weapons = [];
+}
+
+Player.prototype.surviveDay = function() {
+  this.hunger--;
 }
 
 module.exports = Player
--- js/test/playerSpec.js
+++ js/test/playerSpec.js
@@ -1,6 +1,5 @@
-const chai = require('chai')
+const expect = require('chai').expect;
 const Player = require('../public/src/player.js')
-const expect = chai.expect;
 
 describe('Player', function() {
   var player;
@@ -10,8 +9,27 @@ describe('Player', function() {
   })
 
   describe('Initialisation', function() {
-    it('has starts with 100 health', function() {
+    it('starts with 100 health', function() {
       expect(player.health).equal(100);
+    })
+
+    it('starts with no weapons', function() {
+      expect(player.weapons).to.deep.equal([]);
+    })
+
+    it('starts with 10 points of hunger', function() {
+      expect(player.hunger).equal(10);
+    })
+
+    it('starts with 10 points of thirst', function() {
+      expect(player.thirst).equal(10);
+    })
+  })
+
+  describe('#surviveDay', function() {
+    it('the player loses hunger points after a day', function() {
+      player.surviveDay();
+      expect(player.hunger).equal(9);
     })
   })
 })
